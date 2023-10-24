8fdf5b97e83dfc73aadc08657aeee0f2ecac6b7b
OluEkele <ekeleolu1@gmail.com>
2023-10-24 15:37:30 +0100

    deploy

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music3",
+        src: "music-1.mp3"
+    },
+    {
+        name: "Ikson Anywhere - Ikson",
+        artist: "Audio Library",
+        img: "music2",
+        src: "music-2.mp3"
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img: "music1",
+        src: "music-3.mp3"
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "music4",
+        src: "music-4.mp3"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Ray",
+        img: "music5",
+        src: "music-5.mp3"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NSC Release",
+        img: "music6",
+        src: "music-6.mp3"
+    }
+]
